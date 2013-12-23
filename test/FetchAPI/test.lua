d8ae001dc888b2c2122db47e0aa1b217ca651121
Anaminus <anaminus@no-email>
2013-12-23 14:56:27 -0600

    added unit test for FetchAPI

---
+++ test/FetchAPI/test.lua
@@ -0,0 +1,62 @@
+local versions = {
+	{'version-87de5333d4254860'};
+	{'version-38293b7e060d4866'};
+	{'version-12cd4783f01a48cf'};
+	{'version-19c5d0ac8e9b47c4','version-e8936cd10a7748e5'};
+}
+
+local function read(name)
+	local f = assert(io.open(name,'rb'))
+	local r = f:read('*a')
+	f:close()
+	return r
+end
+
+local function teq(a,b)
+	for k,v in pairs(a) do
+		if b[k] ~= v then
+			return false
+		end
+	end
+	for k,v in pairs(b) do
+		if a[k] ~= v then
+			return false
+		end
+	end
+	return true
+end
+
+local FetchAPI = require 'FetchAPI'
+
+local function test(v)
+	local verPlayer = v[1]
+	local verStudio = v[2]
+
+	print("Testing",verPlayer,verStudio)
+
+	local aDump = read(verPlayer .. '.dmp')
+	local aIndex = require(verPlayer)
+	local aExec = verPlayer .. '/' .. read(verPlayer .. '.txt')
+
+	local bDump,bIndex,bExec = FetchAPI(verPlayer,verStudio)
+	if not bDump then
+		print("FetchAPI failed:",bIndex)
+	end
+
+	assert(bDump==aDump,"APIDump test failed: Dump does not match")
+	assert(teq(bIndex,aIndex),"ExplorerIndex test failed: ExplorerIndex table contents do not match")
+	assert(bExec:match('.*/(.+)$')==aExec:match('.*/(.+)$'),"Exec test failed: Executable names do not match")
+	print()
+end
+
+print("Uncached test")
+for i = 1,#versions do
+	test(versions[i])
+end
+
+print("Cached test")
+for i = 1,#versions do
+	test(versions[i])
+end
+
+print("Test Finished")
